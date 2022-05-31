a5f181ee3522df1bce7f22b4e3fd956f8191b77d
i-got-sota <sotaso.71@gmail.com>
2022-05-31 18:40:46 +0900

    テンプレート作成

---
+++ Object/object.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const data = ["JavaScript", "Kotlin", "Python", "Ruby", "Swift", "Elixir", "Fortran", "C++", "C#", "Go",
+        "Objective-C", "PHP", "Perl", "COBOL", "Scratch", "TypeScript", "Visual Basic", "Dart", "Java", "Rust"];
+
+const emailAddress = ["example444@example.com", "9a-fg@example.com", "a--a-a@ppp.com", "-a@example.com", "a@example.com", "p+ppp@example.com",
+                        "example@a..jp", "admin@example.com", "P-admin@example.com", "enmene@aDmin.jp", "eA@aaa.jp", "e-a@co.jp", "a.aa.aaa"];
